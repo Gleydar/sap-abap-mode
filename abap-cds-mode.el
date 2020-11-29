6b16cd4528e4d12b84656a21851dd67e962d61fc
Marian Piatkowski <marianpiatkowski@web.de>
2020-11-29 15:30:23 +0100

    add header to file 'abap-cds-mode.el'

--- abap-cds-mode.el
+++ abap-cds-mode.el
@@ -1,8 +1,27 @@
+;;; abap-cds-mode.el --- ABAP CDS Major Mode    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Marian Piatkowski
+
+;; Author: Marian Piatkowski <marianpiatkowski@web.de>
 ;; Keywords: SAP ABAP CDS Mode, Emacs
 
-;; TODOs
-;; - maybe rename abap-cds-mode-hook
-;; - maybe rename abap-cds-mode
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Major mode for SAP ABAP CDS (CDS = Core Data Services)
+
+;;; Code:
 
 (require 'abap-mode)
 
